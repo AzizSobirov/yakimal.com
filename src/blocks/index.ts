3789715ebd9a735f6667390c75b3776a9c859b77
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-06-15 13:35:51 +0500

    feat: add Application and News components with form functionality and styling

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,3 +1,5 @@
 export { default as About } from "./About.astro";
+export { default as Application } from "./Application.astro";
 export { default as Hero } from "./Hero.astro";
+export { default as News } from "./News.astro";
 export { default as OurProducts } from "./OurProducts.astro";
