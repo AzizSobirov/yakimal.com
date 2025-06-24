abd956f5373821142cb0e9f89c1ec34e3de5fe20
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-06-24 22:58:25 +0500

    feat: add Branding, Advantages, and Stages components; update routing and fix links in Header and Footer

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,5 +1,7 @@
 export { default as About } from "./About.astro";
+export { default as Advantages } from "./Advantages.astro";
 export { default as Application } from "./Application.astro";
+export { default as Branding } from "./Branding.astro";
 export { default as Catalog } from "./Catalog.astro";
 export { default as Follow } from "./Follow.astro";
 export { default as Geography } from "./Geography.astro";
@@ -13,6 +15,7 @@ export { default as Product } from "./Product.astro";
 export { default as ProductRecipes } from "./ProductRecipes.astro";
 export { default as Products } from "./Products.astro";
 export { default as Recipes } from "./Recipes.astro";
+export { default as Stages } from "./Stages.astro";
 export { default as Timeline } from "./Timeline.astro";
 export { default as WhyUs } from "./WhyUs.astro";
 export { default as Yakimal } from "./Yakimal.astro";
